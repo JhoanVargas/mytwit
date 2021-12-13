7b2d4f1af70787b81ebf3dfd6c6598aae6069f0c
JhoanVargas <jhoanjjvp@gmail.com>
2021-12-13 18:59:17 -0500

    Registro nuevo prueba

--- routers/registro.go
+++ routers/registro.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/JhoanVargas/mytwit/bd"
@@ -11,6 +12,7 @@ import (
 func Registro(w http.ResponseWriter, r *http.Request) {
 	var t models.Usuario
 	err := json.NewDecoder(r.Body).Decode(&t)
+	fmt.Println(len(t.Password))
 	if err != nil {
 		http.Error(w, "Error en los datos recibidos"+err.Error(), 400)
 		return
