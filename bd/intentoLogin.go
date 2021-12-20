5d355a7c73d03ca60446680ff379de13e921cb0f
JhoanVargas <jhoanjjvp@gmail.com>
2021-12-20 16:31:10 -0500

    VerPerfil

---
+++ bd/intentoLogin.go
@@ -0,0 +1,24 @@
+package bd
+
+import (
+	"github.com/JhoanVargas/mytwit/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func IntentoLogin(email string, password string) (models.Usuario, bool) {
+	usu, encontrado, _ := ChequeoYaExisteUsuario(email)
+
+	if encontrado == false {
+		return usu, false
+	}
+
+	passwordBytes := []byte(password)
+	passwordBd := []byte(usu.Password)
+
+	err := bcrypt.CompareHashAndPassword(passwordBd, passwordBytes)
+
+	if err != nil {
+		return usu, false
+	}
+	return usu, true
+}
